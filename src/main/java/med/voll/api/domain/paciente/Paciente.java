1800dd9356ccfd3ae36ffd547d6fc35c7d7714ee
Julia Hown <juliahown87@gmail.com>
2024-12-20 15:35:11 -0300

    feat: new class Usuario, ajuste nos caminhos e pom.xml

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -1,13 +1,11 @@
 package med.voll.api.paciente;
 
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import med.voll.api.endereco.DadosEndereco;
-import med.voll.api.endereco.Endereco;
+import med.voll.api.domain.endereco.Endereco;
 
 @Table(name = "pacientes")
 @Entity(name = "Paciente")
